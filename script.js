5aa0b9a8dc51dd1b89bff9a6f4b1610a595bc5b1
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-17 12:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let length=shortHex.length-3;
+	let output="#";
+	let isCapital=false;
+	while (length<shortHex.legth) {
+		let aascci=shortHex[i].charChodeAt(0)
+		if(aascci>=60 && asscci<=90){
+			isCapital=true;
+		}
+		output=output+shortHex[i]+shortHex[i]
+
+		length++;
+	}
+
+	return isCapital?output.toUpperCase():output
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
